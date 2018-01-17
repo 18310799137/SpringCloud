5cc66aa3034108ebd84bc53c1a7ab0deb5b86a5a
18310799137 <18310799137@163.com>
2018-01-17 13:46:56 +0800

    Sleuth 服务跟踪组件

---
+++ SleuthClientB/src/main/java/com/zgh/sleuth/client/b/SleuthClientB/App.java
@@ -0,0 +1,13 @@
+package com.zgh.sleuth.client.b.SleuthClientB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
